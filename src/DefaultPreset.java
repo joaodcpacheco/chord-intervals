86d0f6ae5cf2ce4c0c3de068023759c9c4c0e421
Your Name <you@example.com>
2020-11-22 21:43:43 +0000

    added more tunings

--- src/DefaultPreset.java
+++ src/DefaultPreset.java
@@ -77,57 +77,102 @@ public enum DefaultPreset
 	// D4 B3 G3 D3 G2 D2
 	GUITAR_OPEN_G("Open G;6;21;50-47-43-38-31-26" + Const.GMARKS),
 
+	// E4 B3 G3 D3 A2 E2
+	GUITAR_ALL_FOURTHS("All-fourths tuning;6;22;53-48-43-38-33-28" + Const.GMARKS),
+	
+	// G4 E4 A3 D3 G2 C2
+	GUITAR_NST("New standard tuning;6;22;55-52-45-38-31-24" + Const.GMARKS),
+
 	// E4 B3 G3 D3 A2 E2 B1
 	GUITAR7_B_STANDARD("7-string B standard;7;24;52-47-43-38-33-28-23" + Const.GMARKS24),
 
 	// E4 B3 G3 D3 A2 E2 A1
-	GUITAR7_DROP_A("7-string Drop A;7;24;52-47-43-38-33-28-21" + Const.GMARKS24),
+	GUITAR7_DROP_A("7-string drop A;7;24;52-47-43-38-33-28-21" + Const.GMARKS24),
 
 	// D#4 A#3 F#3 C#3 G#2 D#2 A#1
 	GUITAR7_A$_STANDARD("7-string A# standard;7;24;51-46-42-37-32-27-22" + Const.GMARKS24),
 
 	// D#4 A#3 F#3 C#3 G#2 D#2 G#1
-	GUITAR7_DROP_G$("7-string Drop G#;7;24;51-46-42-37-32-27-20" + Const.GMARKS24),
+	GUITAR7_DROP_G$("7-string drop G#;7;24;51-46-42-37-32-27-20" + Const.GMARKS24),
 
 	// D4 A3 F3 C3 G2 D2 A1
 	GUITAR7_A_STANDARD("7-string A standard;7;24;50-45-41-36-31-26-21" + Const.GMARKS24),
 
 	// D4 A3 F3 C3 G2 D2 G1
-	GUITAR7_DROP_G("7-string Drop G;7;24;50-45-41-36-31-26-19" + Const.GMARKS24),
+	GUITAR7_DROP_G("7-string drop G;7;24;50-45-41-36-31-26-19" + Const.GMARKS24),
 
 	// C#3 G#3 E3 G2 F#2 C#2 G#1
 	GUITAR7_G$_STANDARD("7-string G# standard;6;24;49-44-40-35-30-25-20" + Const.GMARKS24),
 
 	// C#3 G#3 E3 G2 F#2 B1 F#1
-	GUITAR7_DROP_F$("7-string Drop F#;6;24;49-44-40-35-30-23-18" + Const.GMARKS24),
+	GUITAR7_DROP_F$("7-string drop F#;6;24;49-44-40-35-30-23-18" + Const.GMARKS24),
 
 	// C3 G3 D#3 A#2 F2 C2 G1
 	GUITAR7_G_STANDARD("7-string G standard;6;24;48-43-39-34-29-24-19" + Const.GMARKS24),
 
 	// C3 G3 D#3 A#2 F2 A#1 F1
-	GUITAR7_DROP_F("7-string Drop F;6;24;48-43-39-34-29-22-17" + Const.GMARKS24),
+	GUITAR7_DROP_F("7-string drop F;6;24;48-43-39-34-29-22-17" + Const.GMARKS24),
 
 	// E4 B3 G3 D3 A2 E2 B1 F#1
 	GUITAR8_F$_STANDARD("8-string F# standard;8;24;52-47-43-38-33-28-23-18" + Const.GMARKS24),
 
 	// E4 B3 G3 D3 A2 E2 B1 E1
-	GUITAR8_DROP_E("8-string Drop E;8;24;52-47-43-38-33-28-23-16" + Const.GMARKS24),
+	GUITAR8_DROP_E("8-string drop E;8;24;52-47-43-38-33-28-23-16" + Const.GMARKS24),
 
 	// D#4 A#3 F#3 C#3 G#2 D#2 A#1 F1
 	GUITAR8_F_STANDARD("8-string F standard;8;24;51-46-42-37-32-27-22-17" + Const.GMARKS24),
 
 	// D#4 A#3 F#3 C#3 G#2 D#2 A#1 D#1
-	GUITAR8_DROP_D$("8-string Drop D#;8;24;51-46-42-37-32-27-22-15" + Const.GMARKS24),
+	GUITAR8_DROP_D$("8-string drop D#;8;24;51-46-42-37-32-27-22-15" + Const.GMARKS24),
 
 	// E4 B3 G3 D3 A2 E2 B1 F#1 C#1
 	GUITAR9_C$_STANDARD("9-string C# standard;9;24;52-47-43-38-33-28-23-18-13" + Const.GMARKS24),
 
 	// E4 B3 G3 D3 A2 E2 B1 F#1 B0
-	GUITAR9_DROP_B("9-string Drop B;9;24;52-47-43-38-33-28-23-18-11" + Const.GMARKS24),
+	GUITAR9_DROP_B("9-string drop B;9;24;52-47-43-38-33-28-23-18-11" + Const.GMARKS24),
 
+	// G2 D2 A1 E1
+	BASS_E_STANDARD("Bass standard tuning;4;20;31-26-21-16" + Const.BMARKS),
+	
+	// G2 D2 A1 D1
+	BASS_DROP_D("Bass drop D;4;20;31-26-21-14" + Const.BMARKS),
+
+	// F#2 C#2 G#1 D#1
+	BASS_D$_STANDARD("Bass D# standard;4;21;30-25-20-15" + Const.BMARKS),
+
+	// F#2 C#2 G#1 C#1
+	BASS_DROP_C$("Bass drop C#;4;21;30-25-20-13" + Const.BMARKS),
+
+	// F2 C2 G1 D1
+	BASS_D_STANDARD("Bass D standard;4;21;29-24-19-14" + Const.BMARKS),
+
+	// F2 C2 G1 C1
+	BASS_DROP_C("Bass drop C;4;21;29-24-19-12" + Const.BMARKS),
+
+	// E2 B1 F#1 C#1
+	BASS_C$_STANDARD("Bass C# standard;4;22;28-23-18-13" + Const.BMARKS),
+
+	// E2 B1 A#1 B0
+	BASS_DROP_B("Bass drop B;4;22;28-23-18-11" + Const.BMARKS),
+
+	// D#2 A#1 F1 C1
+	BASS_C_STANDARD("Bass C standard;4;22;27-22-17-12" + Const.BMARKS),
+
+	// D#2 A#1 F1 A#0
+	BASS_DROP_A$("Bass drop A#;4;22;27-22-17-10" + Const.BMARKS),
+
+	// G2 D2 A1 E1 B0
+	BASS5_B_STANDARD("5-string bass B standard;5;20;31-26-21-16-11" + Const.BMARKS),
+
+	// G2 D2 A1 E1 A0
+	BASS5_DROP_A("5-string bass drop A;5;20;31-26-21-16-9" + Const.BMARKS),
+	
 	// A4 E4 C4 G4
-	UKULELE("Ukulele standard tuning;4;18;57-52-48-55" + Const.UMARKS),;
+	UKULELE_STANDARD("Ukulele standard tuning;4;18;57-52-48-55" + Const.UMARKS),
 
+	// A4 E4 A3 G4
+	UKULELE_DROP_A("Ukulele drop A;4;18;57-52-45-55" + Const.UMARKS),;
+	
 	String specs;
 
 	DefaultPreset(String specs)
@@ -156,6 +201,7 @@ public enum DefaultPreset
 	{
 		private static final String GMARKS = ";3-5-7-9-12-15-17-19-21";
 		private static final String GMARKS24 = ";3-5-7-9-12-15-17-19-21-24";
+		private static final String BMARKS = ";3-5-7-9-12-15-17-19";
 		private static final String UMARKS = ";5-7-10-12";
 	}
 }
